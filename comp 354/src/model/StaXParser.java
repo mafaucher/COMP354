365cab5f26fdf5cc9e87b560c2a1750b2a533717
taspaulin@gmail.com <taspaulin@gmail.com@dcea7918-6770-44ad-ae98-f87e21d40bd0>
2012-01-22 01:20:56 +0000

    Tom's Update. Includes the follow:
    - Code now writes out a file
    - Contains formatted data based on XML doc (renamed to tasklist.xml)
    
    Need to clean up a bit, but all works and can be tested.

--- comp 354/src/model/StaXParser.java
+++ comp 354/src/model/StaXParser.java
@@ -1,7 +1,12 @@
 package model;
 
+import java.io.File;
+
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -26,7 +31,6 @@ public class StaXParser {
 	static final String DEADLINE = "deadline";
 	static final String PERSONASSIGNED = "personassigned";
 	static final String COMPLETION = "completion";
-	
 
 	@SuppressWarnings({ "unchecked", "null" })
 	public List<task> readConfig(String configFile) {
@@ -130,5 +134,130 @@ public class StaXParser {
 		}
 		return tasks;
 	}
+	
+    public static void processOutput( List<task> taskList )
+    {	
+    	try{
 
+    		ArrayList uniquenames = new ArrayList(); 
+    		ArrayList<Person> people = new ArrayList<Person>();
+    		
+    		System.out.println("Writing Output File...");
+    		
+    		int n = 0;
+    		int longestnamelen = 0;
+    		int longesthourslen = 0;	
+    		int longestprojlistlen = 0;	
+    		
+    		for (task task : taskList) {
+    			String[] names =  task.getPersonassigned().toString().split( ",\\s*" );
+
+    			for ( String string : names ){
+    				int onproj = names.length;
+    				double timedistro = (Double.parseDouble(task.getDuration()) / onproj);
+
+			    	if(string.length() > longestnamelen){
+			    		longestnamelen = string.length();    			    		
+			    	}
+			    	
+
+    			    if(!uniquenames.contains(string)){
+    			    	Person temp = new Person();
+    			    	
+    			    	temp.setName(string);
+    			    	temp.setTotalHours(timedistro);
+    			    	temp.setProjects(task.getIdentifier());
+    	    	
+    			    	people.add(temp);    
+    			    	uniquenames.add(string);
+    			    }
+    			    else{
+    			    	Iterator iterator = people.iterator();
+    					
+    			    	for ( Person temp : people ){
+    			    			if( string.equals(temp.getName())){
+    			    				temp.setTotalHours(timedistro);
+    			    				temp.setProjects(task.getIdentifier());
+    			    				
+    						    	if(temp.getProjects().toString().length() > longestprojlistlen){
+    						    		longestprojlistlen = temp.getProjects().toString().length();    
+    						    	}
+    						    	
+    						    	if(Double.toString(temp.getTotalHours()).length() > longesthourslen){
+    						    		longesthourslen = Double.toString(temp.getTotalHours()).length();
+    						    	}
+    			    			}
+    			    	}
+    			    	
+    			    }
+    			}    			
+    		}
+    		
+    		
+    		String[] headers = {"Name","Total Hours","Project List"};
+    		
+    		// Write out the file header
+    		String content = "Name";
+    		
+    		int i = 0;
+    		int currnamelen = 0;
+    		int currhourslen = 0;
+    		int headerlen = 0;
+    		int projlistlen = 0;
+    		
+    		for (i=0; i<(longestnamelen-3); i++){
+    			  content += " ";
+    		}
+    		
+    		content += "| Total Hours | Projects" + "\n" +
+					 "--------------------------------------" + "\n";
+
+	    	for ( Person temp : people ){
+	    		currnamelen = temp.getName().length();
+
+	    		content += temp.getName();
+	    		
+	     		for (i=0; i<((longestnamelen-currnamelen)+1); i++){
+	    			  content += " ";
+	    		}
+	    		
+	     		String roundedHours = String.format("%.2f", temp.getTotalHours());
+	    		content +="| ";
+    			    		
+	     		content += roundedHours;
+	    		currhourslen = Double.toString(temp.getTotalHours()).length();
+
+	    		currhourslen = headers[1].length();
+
+	    		
+	     		for (i=0; i<((longesthourslen-currhourslen)-1); i++){
+	    			  content += " ";
+	    		}
+	     		content += "| " + temp.getProjects() + "\n";
+
+	    	}
+    	
+
+    		
+
+    		File file = new File("people.txt"); // Name of the output file
+ 
+    		// If the file does not exists, create it.
+    		if(!file.exists()){
+    		   file.createNewFile();
+    		}
+ 
+    		FileOutputStream fop=new FileOutputStream(file);
+		//get the content in bytes
+    		fop.write(content.getBytes());
+    		fop.flush();
+	        fop.close();
+ 
+            System.out.println("Output File Successfully Written.");
+ 
+    	}catch(IOException e){
+    		e.printStackTrace();
+    	}
+    }
 }
+
--- comp 354/src/model/TestRead.java
+++ comp 354/src/model/TestRead.java
@@ -6,10 +6,20 @@ import model.task;
 
 public class TestRead {
 	public static void main(String args[]) {
+
+		// Create a new StaXParser, read
 		StaXParser read = new StaXParser();
-		List<task> readConfig = read.readConfig("test.xml");
-		for (task task : readConfig) {
-			System.out.println(task);
-		}
+		
+		System.out.println("Reading Input File...");
+		
+		// Read in the XML File containing the task details
+		List<task> readConfig = read.readConfig("tasklist.xml");
+		
+		
+		System.out.println("Input File Successfully Read.");
+		
+		// Output the file detailing the people
+		read.processOutput(readConfig);
 	}
+	
 }
