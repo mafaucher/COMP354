48f8a2e85fc0548c767d6bb919a49e10d4285f7b
dimaaash <dimaaash@dcea7918-6770-44ad-ae98-f87e21d40bd0>
2012-01-31 05:32:52 +0000

    I've included few tests for StaXParser. These tests validate XML input file(s) format. Right now StaXParser has 3 methods: readPeople, readTasks and readSubtasks which read appropriate files. However the parser itself doesn't validate XML files. I think it throws an exception if it can't understand it, but it won't say if XML is valid or not. And if there is a field missing in a XML file the StaXParser doesn't detect it. The tests are using files from test/resources to run. There are 3 XML schema files (PERSON_DTD_SCHEMA.dtd, TASK_DTD_SCHEMA.dtd and SUBTASK_DTD_SCHEMA.dtd). These schema files define structure of our xml files: identity, title, description fields and etc, for each respective file. So the validation is done against those schema files. If an xml is not of that form, it's invalid. Validation will detect if there is a field missing. Say in a xml file there is field "identity" that's missing, then validation will fail for that file. However, it doesn't validate the actual contents of fields. That's what the other tests are for in Person, Task and Subtask classes. So in the future in our main program, the parser should first validate the input files, if they pass it will start parsing them and populating Person,Task and Subtasks objects. However if an object gets data which it detects as invalid it should throw an exception that we should handle in the StaXParser or it should return something to StaXParser to tell it that there is bad data in a file.

---
+++ comp 354/tests/model/StaXParserTests/StaXParserTesting_Person_using_PEOPLE_BAD_XML_FORMAT.java
@@ -0,0 +1,89 @@
+package model.StaXParserTests;
+
+import java.io.*;
+
+import org.w3c.dom.*;
+import org.xml.sax.*;
+
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import static org.junit.Assert.*;
+
+import junit.framework.Assert;
+
+//import model.StaXParser;
+//import model.Task;
+
+//import org.junit.AfterClass;
+import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.junit.runners.Parameterized;
+
+
+//@RunWith(Parameterized.class)
+public class StaXParserTesting_Person_using_PEOPLE_BAD_XML_FORMAT {
+
+	//private static StaXParser __StaXParser__;
+	
+	private static String dataFile = "tests/resources/PEOPLE_BAD_XML_FORMAT.xml";
+	private static String dataFile2 = "tests/resources/PERSON_DTD_SCHEMA.dtd";
+
+	/*
+	public StaXParserTest_using_BAD_XML (	String _dummy_, 	Boolean _b1_) {				
+ 		//__StaXParser__= new StaXParser(); 
+	}
+	
+	@Parameterized.Parameters 
+    public static List<Object[]> data() {
+ 		System.out.println("Testing using file: "+ dataFile + " and using DTD schema: " + dataFile2 );   	
+		System.out.println("___________________________________________________________________________________________________________" );   	
+ 		    	 
+    	 return Arrays.asList(new Object[][] {
+                 { "dummy", true }                                                   
+         });    	 
+    }
+    */	
+	
+	
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		 try{  
+				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();  
+		                      factory.setValidating(true);  
+		                      DocumentBuilder builder = factory.newDocumentBuilder();  
+		                      builder.setErrorHandler(new org.xml.sax.ErrorHandler() {  
+		                           //Ignore the fatal errors  
+		                           public void fatalError(SAXParseException exception)  
+		                                     throws SAXException { }  
+		                                //Validation errors   
+									public void error(SAXParseException e)  throws SAXParseException {  
+										System.out.println("Error at " +e.getLineNumber() + " line.");  
+										System.out.println(e.getMessage());  
+										Assert.assertTrue(!true);
+										//System.exit(0);  
+									}  
+									//Show warnings  
+									public void warning(SAXParseException err)  throws SAXParseException{  
+										System.out.println(err.getMessage());  
+										//System.exit(0);  
+									}  
+								});  
+		                 Document xmlDocument = builder.parse(new FileInputStream(dataFile));  
+		                 DOMSource source = new DOMSource(xmlDocument);  
+		                 StreamResult result = new StreamResult(System.out);  
+		                 TransformerFactory tf = TransformerFactory.newInstance();  
+		                 Transformer transformer = tf.newTransformer();  
+		                 transformer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, dataFile2);  
+		                 transformer.transform(source, result);  
+			}  
+		    catch (Exception e) {  
+				System.out.println(e.getMessage());  
+				Assert.assertTrue(!true);
+		     }      	
+	}
+
+}
