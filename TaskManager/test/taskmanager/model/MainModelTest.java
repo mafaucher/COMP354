d0b10882f66ed3240c95de97e84e4b3aed79dc79
taspaulin@gmail.com <taspaulin@gmail.com@dcea7918-6770-44ad-ae98-f87e21d40bd0>
2012-03-10 22:47:56 +0000

    Completed unit tests for increment 2!

--- TaskManager/test/taskmanager/model/MainModelTest.java
+++ TaskManager/test/taskmanager/model/MainModelTest.java
@@ -13,10 +13,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author t_pauli
- */
 public class MainModelTest {
 
     public MainModelTest() {
